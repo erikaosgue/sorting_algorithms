95820f0daa8c385574d41ee237f0a52123629d4f
erikaosgue <1476@holbertonschool.com>
2020-06-11 19:27:12 +0000

    First version of header

---
+++ 0-bubble_sort.c
@@ -0,0 +1,13 @@
+#include "sort.h"
+
+/**
+ * bubble_sort -sorts an array of integers in ascending order
+ * using the Bubble sort algorithm
+ * @array:
+ * @size:
+ *
+ */
+void bubble_sort(int *array, size_t size)
+{
+
+}
--- sort.h
+++ sort.h
@@ -16,5 +16,7 @@ typedef struct listint_s
     struct listint_s *next;
 } listint_t;
 
+void print_list(const listint_t *list);
+void print_array(const int *array, size_t size);
 
 #endif //f SORT_H
