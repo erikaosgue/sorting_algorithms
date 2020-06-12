965dd7be0919d953c44467250fa6cff14e0b4d59
erikaosgue <1476@holbertonschool.com>
2020-06-12 21:47:00 +0000

    Add task 3: Quick sort

---
+++ 3-quick_sort.c
@@ -0,0 +1,63 @@
+#include "sort.h"
+
+
+
+/**
+ * quick_sort - sorts an array of integers in ascending
+ * order using the Quick sort algorithm
+ *
+ * @array: Array of integers to sort
+ * @size: The size of the array
+ */
+
+void quick_sort(int *array, size_t size)
+{
+	recursion(array, size, array, size);
+}
+
+/**
+ * recursion - sorts an array of integers in ascending
+ * order using Recursion
+ *
+ * @array: Portion of the array to be sort
+ * @size: size of the array
+ * @full_array: The complete array
+ * @full_size: The size of the full_array
+ */
+
+void recursion(int *array, size_t size, int *full_array, size_t full_size)
+{
+
+	int *pivot = NULL;
+	size_t j = 0;
+	int i = -1, value = 0;
+
+	if (size == 0 || array == NULL)
+		return;
+
+	pivot = &array[size - 1];
+	for (j = 0; j < size - 1; j++)
+	{
+		if (array[j] < *pivot)
+		{
+			i++;
+			if (&array[i] != &array[j])
+			{
+				print_array(full_array, full_size);
+				value = array[i];
+				array[i] = array[j];
+				array[j] = value;
+			}
+		}
+	}
+	i++;
+	if (&array[i] != pivot)
+	{
+		value = array[i];
+		array[i] = *pivot;
+		*pivot = value;
+		print_array(full_array, full_size);
+	}
+	recursion(&array[0], i, full_array, full_size);
+	recursion(&array[i + 1], size - (i + 1), full_array, full_size);
+}
