1a361bf4b6a0114dd6a86e3f0c9bdd6f0d163bf2
erikaosgue <1476@holbertonschool.com>
2020-06-12 00:50:10 +0000

    Add task 1: insertion sort list

--- sort.h
+++ sort.h
@@ -11,13 +11,14 @@
  */
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 
 void print_list(const listint_t *list);
 void print_array(const int *array, size_t size);
 void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
 
 #endif /* SORT_H */
